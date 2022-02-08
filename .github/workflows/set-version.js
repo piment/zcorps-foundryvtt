bea050d71b6e25aa06e6d22f01c0bdfdf627052a
Romain MURA <romain@mura.io>
2022-02-08 01:28:09 +0100

    modification de set-version.js

--- .github/workflows/set-version.js
+++ .github/workflows/set-version.js
@@ -3,6 +3,7 @@ const manifest = JSON.parse(fs.readFileSync('system.json', 'utf8'));
 // first argument is node, second is the filename of the script, third is the version we pass in our workflow.
 // expected tag format is 'refs/tags/v{major}.{minor}.{patch}"
 const tagVersion = process.argv[2].split('/').slice(-1)[0]; 
+console.log(`${tagVersion}`);
 if (!tagVersion || !tagVersion.startsWith('v')) {
   console.error(`Invalid version specified: ${tagVersion}`);
   process.exitCode = 1;
