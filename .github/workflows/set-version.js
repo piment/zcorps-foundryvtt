faa38b1b4d0ea6b9584bd367a1d25dad6574ddc4
Romain MURA <romain@mura.io>
2022-02-08 01:39:34 +0100

    Ajout de la MAJ automatique de system.json

--- .github/workflows/set-version.js
+++ .github/workflows/set-version.js
@@ -3,11 +3,14 @@ const manifest = JSON.parse(fs.readFileSync('system.json', 'utf8'));
 // first argument is node, second is the filename of the script, third is the version we pass in our workflow.
 // expected tag format is 'refs/tags/v{major}.{minor}.{patch}"
 const tagVersion = process.argv[2].split('/').slice(-1)[0]; 
+console.log(`${tagVersion}`);
 if (!tagVersion || !tagVersion.startsWith('v')) {
   console.error(`Invalid version specified: ${tagVersion}`);
   process.exitCode = 1;
 } else {
   manifest.version = tagVersion.substring(1); // strip the 'v'-prefix
+  manifest.manifest = `https://github.com/piment/zcorps-foundryvtt/releases/download/${tagVersion}/system.json`;
+  manifest.download = `https://github.com/piment/zcorps-foundryvtt/releases/download/${tagVersion}/archive.zip`
   fs.writeFileSync('system.json', JSON.stringify(manifest, null, 2)); // pretty print JSON back to module.json
   console.log(tagVersion);
 }
